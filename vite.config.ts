31719a04cfa94a2cf251a2d56255c9541554bc7f
unknown <emelgarejox@gmail.com>
2023-11-11 01:48:44 -0500

    build 2

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // base:'/app-front/', //publicar en subcarpeta
-  base:'/',
+  base:'/vite-react-pdf/', //publicar en subcarpeta
+  //base:'/',
 })
